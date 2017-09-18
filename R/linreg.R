a45d31d395d554ed0057d3660063dc150a78d9de
brbatv <barakatbruno@gmail.com>
2017-09-18 19:03:37 +0200

    Extracted X and Y from formula and data input in linreg

--- R/linreg.R
+++ R/linreg.R
@@ -1,4 +1,9 @@
 
 linreg<-function(formula,data){
-  
+  dependent_variable_name<-all.vars(formula)[1]
+  Y<-data[[dependent_variable_name]] # vector of dependent variable
+  X<-model.matrix(formula,data) # matric of independent variables
+  X_n<-model.matrix(formula,data)[,-1] # same without intercept column
+  #f<-Petal.Length ~ Sepal.Width + Sepal.Length
+  return(X)
 }
