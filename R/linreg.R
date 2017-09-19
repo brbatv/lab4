ee0ca724bf3d4ad7a82d17c8a4a76259850ac63c
Alessia De Biase <alede379@student.liu.se>
2017-09-19 14:55:49 +0200

    Functions of linreg()

--- R/linreg.R
+++ R/linreg.R
@@ -3,7 +3,31 @@ linreg<-function(formula,data){
   dependent_variable_name<-all.vars(formula)[1]
   Y<-data[[dependent_variable_name]] # vector of dependent variable
   X<-model.matrix(formula,data) # matric of independent variables
-  X_n<-model.matrix(formula,data)[,-1] # same without intercept column
+  
+  beta<-solve((t(X) %*% X)) %*% t(X) %*% Y
+  
+  #degree of freedom
+  
+  n <- dim(data)[1]
+  p <- dim(data)[2]-1
+  df<- n-p
+  
+  #residual variance
+  
+  res_var <- (t(res) %*% res)/df
+  
+  #variance of the regression coefficients
+  
+  reg_var <- res_var*solve((t(X) %*% X))
+  
+  #t-values for each coefficient
+  
+  t_values <- beta / sqrt(reg_var)
+  
+  #p-value ????'
+  
+  p_value <- pt(beta,df)
+  
   #f<-Petal.Length ~ Sepal.Width + Sepal.Length
   return(X)
 }
