9e4361632e562feab74ff6672d8dcc3112aa7391
brbatv <barakatbruno@gmail.com>
2017-09-19 19:23:00 +0200

    Put everything in a RC class! Just need to complete methods, and tests added

--- R/linreg.R
+++ R/linreg.R
@@ -1,69 +1,74 @@
-#'@export
-linreg<-function(formula,data){
-  
-  
-  # data<-iris
-  # Petal.Lenght <- data$Petal.Length
-  # Sepal.Width  <- data$Sepal.Width
-  # Sepal.Length <- data$Sepal.Length
-  #ex : formula<-data$Petal.Length ~ data$Sepal.Width + data$Sepal.Length
-  
-  
-  dependent_variable_name<-all.vars(formula)[1]
-  Y<-data[[dependent_variable_name]] # vector of dependent variable
-  X<-model.matrix(formula,data) # matric of independent variables
-  
-  ## calculation using QR decomposition
-  qr<-qr(X)
-  beta_hat<-qr.coef(qr,Y) # regression coefficients
-  fitted<-qr.fitted(qr,Y) # fitted values
-  res<-qr.resid(qr,Y) # residuals
-  
-  #degree of freedom
-  
-  n <- dim(data)[1]
-  p <- dim(data)[2]-2
-  df<- n-p
-  
-  #residual variance
-  
-  res_var <- (t(res) %*% res)/df
-  
-  
-  #variance of the regression coefficients
-  
-  reg_var <- (as.numeric(res_var) * solve((t(X) %*% X)))
-  diag(reg_var)
-  
-  #t-values for each coefficient
-  
-  t_values <- beta_hat / sqrt(diag(reg_var))
-  
-  #p-value ????
-  p_value <- pt(fitted,df)
-  
-  #f<-Petal.Length ~ Sepal.Width + Sepal.Length
-  return(X)
-  
-  #first graph
-  plot(fitted,res, ylab="Residuals",xlab="Fitted values of lm", main="Residuals vs Fitted")
-  
-  stand_res <- abs((res-mean(res))/sqrt(var(res)))
-  ggplot(fitted, stand_res)
-  
-  return(beta)
-
-}
-
 #'@exportClass linreg
 #'@export linreg
-#'@import methods
+
 
 linreg<-setRefClass("linreg", 
-                    fields=list(formula="formula",data="data.frame"),
+                    fields=list(formula="formula",data="data.frame",beta_hat="numeric",res="numeric",fitted="numeric"),
                     methods=list(
+                      initialize = function(formula,data){ # this function is using $new <3
+                        formula<<-formula
+                        data<<-data
+                        dependent_variable_name<-all.vars(formula)[1]
+                        
+                        Y<-data[[dependent_variable_name]] # vector of dependent variable
+                        X<-model.matrix(formula,data) # matric of independent variables
+                        qr<-qr(X)
+                        beta_hat<<-qr.coef(qr,Y) # regression coefficients
+                        fitted<<-qr.fitted(qr,Y) #the fitted values
+                        res<<-qr.resid(qr,Y) # residuals
+                        
+                        #degrees of freedom
+                        n <- dim(data)[1]
+                        p <- dim(data)[2]-2
+                        df<- n-p
+                        
+                        res_var <- (t(res) %*% res)/df # residual variance
+                        
+                        #variance of the regression coefficients
+                        reg_var <- (as.numeric(res_var) * solve((t(X) %*% X)))
+                        reg_var<-diag(reg_var)
+                        
+                        #t-values for each coefficient
+                        t_values <- beta_hat / sqrt(diag(reg_var))
+                        
+                        #p-value ????
+                        p_value <- pt(fitted,df)
+                        
+                        
+                      },
                       resid = function(){
-                        return(123456)
+                        
+                        return(res) #done
+                      },
+                      pred = function(){
+                        return(pred) #done
+                        
+                      },
+                      coef=function(){
+                        return(beta_hat) #done
+                        
+                      },
+                      summary=function(){
+                        
+                      },
+                      plot=function(){
+                        #first graph
+                        plot(fitted,res, ylab="Residuals",xlab="Fitted values of lm", main="Residuals vs Fitted")
+                        
+                        stand_res <- abs((res-mean(res))/sqrt(var(res)))
+                        ggplot(fitted, stand_res)
+                        
+                      },
+                      print=function(){
+                        
                       }
                       
                     ))
+
+#dependent_variable_name<-all.vars(formula)[1]
+
+#Y<-data[[dependent_variable_name]] # vector of dependent variable
+#X<-model.matrix(formula,data) # matric of independent variables
+#qr<-qr(X)
+#print(formula)
+#beta_hat<<-qr.coef(qr,Y) # regression coefficients
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+[![Build Status](https://travis-ci.org/brbatv/lab4.svg?branch=master)](https://travis-ci.org/brbatv/lab4)
 # lab4
 lab4
