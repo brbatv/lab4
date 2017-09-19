b98f7d5a8968bb7036fffcdaf700f1f4201a09f2
brbatv <barakatbruno@gmail.com>
2017-09-19 14:46:27 +0200

    Started QR decomposition, waiting for merge

--- R/linreg.R
+++ R/linreg.R
@@ -1,9 +1,16 @@
 
 linreg<-function(formula,data){
+  #ex : f<-Petal.Length ~ Sepal.Width + Sepal.Length
+  
   dependent_variable_name<-all.vars(formula)[1]
   Y<-data[[dependent_variable_name]] # vector of dependent variable
   X<-model.matrix(formula,data) # matric of independent variables
   X_n<-model.matrix(formula,data)[,-1] # same without intercept column
-  #f<-Petal.Length ~ Sepal.Width + Sepal.Length
-  return(X)
+  
+  
+  ## calculation using QR decomposition
+  qr<-qr(X)
+  beta_hat<-qr.coef(qr,Y)
+  
+  return(beta)
 }
