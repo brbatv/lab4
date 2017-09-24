c9357795f55d9338cf53dac918a94dcc15d9b871
Alessia De Biase <alede379@student.liu.se>
2017-09-24 18:56:57 +0200

    Theme created, only logo is missing!

--- R/linreg.R
+++ R/linreg.R
@@ -11,10 +11,95 @@
 #a<-linreg$new(formula=Petal.Length ~ Species,data=iris)
 #plot(lm(iris$Petal.Length ~ iris$Sepal.Width + iris$Sepal.Length))
 
+#coding of the theme
+theme_LIU <- function (base_size = 11, base_family = "sans") 
+{
+  half_line <- base_size/2
+  theme(
+    line = element_line(colour = "black", size = 0.5, 
+                        linetype = 1, lineend = "butt"), 
+    rect = element_rect(fill = "white", colour = "black",
+                        size = 0.5, linetype = 1),
+    text = element_text(family = base_family, face = "plain",
+                        colour = "white", size = base_size,
+                        lineheight = 0.9,  hjust = 0.5,
+                        vjust = 0.5, angle = 0, 
+                        margin = margin(), debug = FALSE), 
+    axis.line = element_blank(), 
+    axis.text = element_text(size = rel(1), colour = "grey30"),
+    axis.text.x = element_text(margin = margin(t = 0.8*half_line/2), 
+                               vjust = 1), 
+    axis.text.y = element_text(margin = margin(r = 0.8*half_line/2),
+                               hjust = 1),
+    axis.ticks = element_line(colour = "grey20"),
+    axis.ticks.length = unit(half_line/2, "pt"), 
+    axis.title.x = element_text(size = rel(1.2),margin = margin(t = half_line,
+                                                                b = half_line*1.5)),
+    axis.title.y = element_text(size = rel(1.2), angle = 90, 
+                                margin = margin(r = half_line*1.5,
+                                                l = half_line)),
+    axis.title.x.top = element_blank(),
+    axis.title.y.right = element_blank(),
+    axis.text.x.top = element_blank(),
+    axis.text.y.right = element_blank(),
+    axis.line.x = element_blank(),
+    axis.line.y = element_blank(),
+    
+    legend.background = element_rect(colour = NA), 
+    legend.margin = margin(6,6,6,6),
+    legend.key = element_rect(fill = "grey95", colour = "white"),
+    legend.key.size = unit(1.2, "lines"), 
+    legend.key.height = NULL,
+    legend.key.width = NULL, 
+    legend.text = element_text(size = rel(0.8)),
+    legend.text.align = NULL,
+    legend.title = element_text(hjust = 0), 
+    legend.title.align = NULL, 
+    legend.position = "right", 
+    legend.direction = NULL,
+    legend.justification = "center", 
+    legend.box = NULL, 
+    legend.spacing = NULL,
+    legend.spacing.x = NULL,
+    legend.spacing.y = NULL,
+    legend.box.margin = NULL,
+    legend.box.background = NULL,
+    legend.box.spacing = NULL,
+    
+    panel.background = element_rect(fill = "white", colour = NA),
+    panel.border = element_blank(), 
+    panel.grid.major = element_line(colour = "grey95"), 
+    panel.grid.minor = element_line(colour = "grey95", size = 0.25), 
+    #panel.margin = unit(half_line, "pt"), 
+    panel.margin.x = NULL, 
+    panel.margin.y = NULL, panel.ontop = FALSE, 
+    panel.spacing = element_blank(), 
+    panel.spacing.x = element_blank(), 
+    panel.spacing.y = element_blank(), 
+    plot.subtitle = element_blank(), 
+    plot.caption = element_blank(), 
+    
+    
+    strip.background = element_rect(fill = "grey85", colour = NA),
+    strip.text = element_text(colour = "grey10", size = rel(0.8)),
+    strip.text.x = element_text(size=1), 
+    strip.text.y = element_text(angle = -90, 
+                                margin = margin(l = half_line, 
+                                                r = half_line)),
+    strip.switch.pad.grid = unit(0.1, "cm"),
+    strip.switch.pad.wrap = unit(0.1, "cm"), 
+    strip.placement = "outside" ,
+    
+    plot.background = element_rect(fill = "mediumturquoise"), 
+    plot.title = element_text(size = rel(2), 
+                              margin = margin(b = half_line*2.5, l=half_line)),
+    plot.margin = margin(0.8,0.8,0.8,0.8,"cm"),
+    complete = TRUE)
+}
 
 a<-linreg$new(formula=Petal.Length ~ Sepal.Width + Sepal.Length,data=iris)
 linreg<-setRefClass("linreg",
-                    fields=list(formula="formula",data="data.frame",beta_hat="numeric",res="numeric",fitted="numeric",name_of_data_input="character"),
+                    fields=list(formula="formula",data="data.frame",beta_hat="numeric",res="numeric",fitted="numeric",name_of_data_input="character",df="numeric",reg_var="numeric",t_values="numeric",p_value="numeric"),
                     methods=list(
                       initialize = function(formula,data){ # this function is using $new <3
                         formula<<-formula
@@ -32,18 +117,18 @@ linreg<-setRefClass("linreg",
                         #degrees of freedom
                         n <- dim(data)[1]
                         p <- dim(data)[2]-2
-                        df<- n-p
+                        df<<- n-p
                         
                         res_var <- (t(res) %*% res)/df # residual variance
                         
                         #variance of the regression coefficients
-                        reg_var <- diag(as.numeric(res_var) * solve((t(X) %*% X)))
+                        reg_var <<- diag(as.numeric(res_var) * solve((t(X) %*% X)))
                         
                         #t-values for each coefficient
-                        t_values <- beta_hat / sqrt(reg_var)
+                        t_values <<- beta_hat / sqrt(reg_var)
                         
                         #p-value for each coefficient
-                        p_value<-2*pt(abs(t_values),df,lower.tail = FALSE)
+                        p_value<<-2*pt(abs(t_values),df,lower.tail = FALSE)
                         
                         
                       },
@@ -67,14 +152,20 @@ linreg<-setRefClass("linreg",
                       },
                       plot=function(){
                         
+                        logo <- image_read('http://www.ida.liu.se/mall11/images/logo-sv.png')
+                        g_pic<- rasterGrob(logo,interpolate=TRUE)
+                        
                         phras<- paste("ln(",format(formula),")")
                         #first graph
-                        theme_update(plot.title = element_text(hjust = 0.5))
-                        ggplot(data.frame(fitted,res),aes(y=res,x=fitted))+geom_point(shape=1,size=3)+xlab(paste("Fitted values", phras, sep = "\n"))+ ylab("Residuals")+ ggtitle("Residuals vs Fitted") +geom_hline(yintercept=0, linetype="dashed")+geom_smooth(span = 1.5,colour="red",method="loess",se=FALSE)
+                        x_max<-max(fitted)
+                        y_max<-max(res)
+                        ggplot(data.frame(fitted,res),aes(y=res,x=fitted))+geom_point(shape=1,size=3)+xlab(paste("Fitted values", phras, sep = "\n"))+ ylab("Residuals")+ ggtitle("Residuals vs Fitted") +geom_hline(yintercept=0, linetype="dashed")+geom_smooth(span = 1.5,colour="turquoise4",method="loess",se=FALSE)+theme_LIU()+annotation_custom(g_pic, xmin=x_max-2, xmax=x_max-1, ymin=y_max-6, ymax=y_max-1)
                         
                         #second graph
                         stand_res <- sqrt(abs((res-mean(res))/sqrt(var(res))))
-                        ggplot(data.frame(fitted,stand_res),aes(y=stand_res,x=fitted))+geom_point(shape=1)+xlab(paste("Fitted values",phras, sep = "\n"))+ ylab(expression(sqrt(abs("Standardized residuals")))) + ggtitle("Scale-Location")+geom_smooth(span = 1.5,colour="red",method="loess",se=FALSE)
+                        x_max<-max(fitted)
+                        y_max<-max(stand_res)
+                        ggplot(data.frame(fitted,stand_res),aes(y=stand_res,x=fitted))+geom_point(shape=1)+xlab(paste("Fitted values",phras, sep = "\n"))+ ylab(expression(sqrt(abs("Standardized residuals")))) + ggtitle("Scale-Location")+geom_smooth(span = 1.5,colour="turquoise4",method="loess",se=FALSE)+theme_LIU()+annotation_custom(g_pic, xmin=x_max-2, xmax=x_max-1, ymin=y_max-6, ymax=y_max-1)
                           
                       },
                       print=function(){
